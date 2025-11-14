46920b4717d7e96a74dfb7c625419e927e489ca2
Joan-bbs <liuxing0726@gmail.com>
2025-11-14 20:34:22 +0800

    更新 diy.sh

--- diy.sh
+++ diy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # 修改默认 IP
-sed -i 's/192.168.1.1/192.168.2.200/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# bash 替换 ash
-sed -i '1s/ash/bash/' package/base-files/files/etc/passwd
+# 修改本地时间格式的显示
+sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
-# 生成编译时间
-date "+%Y-%m-%d %H:%M:%S %z" >> package/base-files/files/etc/build_date
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2-luci
