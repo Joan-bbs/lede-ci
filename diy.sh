84942c898f90f8d68c70e4c2f09a0c17856dff74
y9858 <53130835+y9858@users.noreply.github.com>
2025-11-14 20:20:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # 修改默认 IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.200/g' package/base-files/files/bin/config_generate
 
 # bash 替换 ash
 sed -i '1s/ash/bash/' package/base-files/files/etc/passwd
 
 # 生成编译时间
 date "+%Y-%m-%d %H:%M:%S %z" >> package/base-files/files/etc/build_date
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
