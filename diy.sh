059437ae909140367c18246fa28b2885df7e402e
y9858 <53130835+y9858@users.noreply.github.com>
2025-11-14 14:18:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# 修改默认主题
-git clone --depth 1 https://github.com/y9858/luci-theme-opentomcat package/luci-theme-opentomcat
-sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
-
 # 修改默认 IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # bash 替换 ash
 sed -i '1s/ash/bash/' package/base-files/files/etc/passwd
-
-# ttyd 免帐号登录
-sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 生成编译时间
 date "+%Y-%m-%d %H:%M:%S %z" >> package/base-files/files/etc/build_date
